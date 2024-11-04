44cf1cd6905f22e6fb072602530b1bd282f48c31
Tudoumi127 <123220803+Tudoumi127@users.noreply.github.com>
2024-11-03 20:59:30 -0800

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -20,3 +20,54 @@ if(ctx){
 }
 
 app.append(canvas);
+
+const clear = document.createElement("button");
+clear.innerHTML = "Clear Canvas";
+app.append(clear);
+
+//functions
+
+let drawing = false;
+let x = 0;
+let y = 0;
+
+canvas.addEventListener("mousedown", (pos) => 
+{
+    x = pos.offsetX;
+    y = pos.offsetY;
+    drawing = true;
+});
+
+canvas.addEventListener("mousemove", (pos) =>
+{
+    if (drawing){
+        drawLine(ctx, x, y, pos.offsetX, pos.offsetY);
+        x = pos.offsetX;
+        y = pos.offsetY;
+    }
+});
+
+canvas.addEventListener("mouseup", (pos) => 
+{
+    if(drawing){
+        drawLine(ctx, x, y, pos.offsetX, pos.offsetY);
+        drawing = false;
+    }
+});
+
+function drawLine(context, x1, y1, x2, y2){
+    context.beginPath();
+    context.strokeStyle = "black";
+    context.lineWidth = 1;
+    context.moveTo(x1, y1);
+    context.lineTo(x2, y2);
+    context.stroke();
+    context.closePath();
+}
+
+clear.addEventListener("mousedown", () => 
+{
+    if(ctx) {
+        ctx.fillRect(0, 0, 256, 256);
+    }
+});
