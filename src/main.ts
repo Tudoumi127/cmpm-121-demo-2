faf7ee0fc9590675d6a285dc240f8f08746b02fd
Tudoumi127 <123220803+Tudoumi127@users.noreply.github.com>
2024-11-03 20:44:28 -0800

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,22 @@
 import "./style.css";
 
-const APP_NAME = "K we're having issues";
+const APP_NAME = "I'm dying actually";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
 app.innerHTML = APP_NAME;
+
+const header = document.createElement("h1");
+header.innerHTML = "Demo 2";
+app.append(header);
+
+const canvas = document.createElement("canvas");
+const ctx = canvas.getContext("2d");
+if(ctx){
+    ctx.canvas.width = 256;
+    ctx.canvas.height = 256;
+    ctx.fillStyle = "white";
+    ctx.fillRect(0, 0, 256, 256);
+}
+
+app.append(canvas);
