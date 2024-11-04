1d75498c571f59dbaf768baee1557bf1f26faa9b
Tudoumi127 <123220803+Tudoumi127@users.noreply.github.com>
2024-11-03 21:02:15 -0800

    clean up

--- src/main.ts
+++ src/main.ts
@@ -11,13 +11,13 @@ header.innerHTML = "Demo 2";
 app.append(header);
 
 const canvas = document.createElement("canvas");
-const ctx = canvas.getContext("2d");
-if(ctx){
-    ctx.canvas.width = 256;
-    ctx.canvas.height = 256;
-    ctx.fillStyle = "white";
-    ctx.fillRect(0, 0, 256, 256);
-}
+canvas.width = 256;
+canvas.height = 256;
+
+const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, 256, 256);
+
 
 app.append(canvas);
 
